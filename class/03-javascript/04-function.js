9c7bbd7554680836a9738239af42320853722ee2
JJHaEun <yj05037@gmail.com>
2023-05-02 21:36:37 +0900

    인증번호 숫자 랜덤색 적용

--- class/03-javascript/04-function.js
+++ class/03-javascript/04-function.js
@@ -1,5 +1,8 @@
 const send = () => {
-  document.getElementById("random").innerText = String(
-    Math.floor(Math.random() * 1000000)
-  ).padStart(6, "0");
+  const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+  const random = document.getElementById("random");
+  random.innerText = token;
+  random.style.color = "#" + token;
+  //  random.style.color =  "#"+(parseInt(Math.random()*0xffffff)).toString(16);
+  // 헥스코드는 6자리. #이 붙은 형태이니 위에서 6자리를 만든 token에 #만 붙여주면 됨. 그럼 token이라는 숫자사체가 헥스코드가 됨.
 };
