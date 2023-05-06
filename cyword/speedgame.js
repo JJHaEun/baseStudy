707ed159bcd8bd89d8c0acf579d836be3b7e0788
JJHaEun <yj05037@gmail.com>
2023-05-06 22:28:08 +0900

    특정 클래스 삭제하기, 추가하기, 바꾸기, 랜덤한 시간때 특정 클래스로 변경하기

--- cyword/speedgame.js
+++ cyword/speedgame.js
@@ -1,2 +1,20 @@
-const screen = document.getElementById("screen");
-const result = document.getElementById("result");
+const startGame = () => {
+  const screen = document.getElementById("screen");
+  const result = document.getElementById("result");
+
+  if (screen.classList.contains("wait")) {
+    // 대기
+    // screen.classList.remove("wait");
+    // screen.class.add("ready");
+    screen.classList.replace("wait", "ready");
+    screen.textContent = "녹색이 되면 클릭하세요";
+    setTimeout(() => {
+      screen.classList.replace("ready", "now");
+      screen.textContent = "지금이에요!!";
+    }, Math.floor(Math.random() * 1000) + 2000); // 3자리의 아무수에 + 2000 = 2000 ~ 3000 사이의 수
+  } else if (screen.classList.contains("ready")) {
+    // 준비
+  } else if (screen.classList.contains("now")) {
+    // 시작
+  }
+};
