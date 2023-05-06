8b83d4159d78198d0c0472d1101c277eaa77806a
JJHaEun <yj05037@gmail.com>
2023-05-07 01:12:40 +0900

    speed게임, 속도구하기

--- cyword/speedgame.js
+++ cyword/speedgame.js
@@ -1,3 +1,5 @@
+let startTime;
+let endTime;
 const startGame = () => {
   const screen = document.getElementById("screen");
   const result = document.getElementById("result");
@@ -9,6 +11,7 @@ const startGame = () => {
     screen.classList.replace("wait", "ready");
     screen.textContent = "녹색이 되면 클릭하세요";
     setTimeout(() => {
+      startTime = new Date();
       screen.classList.replace("ready", "now");
       screen.textContent = "지금이에요!!";
     }, Math.floor(Math.random() * 1000) + 2000); // 3자리의 아무수에 + 2000 = 2000 ~ 3000 사이의 수
@@ -16,5 +19,11 @@ const startGame = () => {
     // 준비
   } else if (screen.classList.contains("now")) {
     // 시작
+    endTime = new Date();
+    result.textContent = `현재: ${endTime - startTime}ms`;
+    screen.classList.replace("now", "wait");
+
+    screen.innerText = "클릭해서 시작하세요!!";
+    // innerText 와 textContent동일
   }
 };
